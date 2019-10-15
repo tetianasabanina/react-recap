5dba1cc6a900a55bdd6a8dcedff5c62ccfdfe33f
Sabanina Tetiana <tatiana08st@gmail.com>
2019-10-15 20:16:23 +0300

    Working with forms

--- src/components/item-add-form/item-add-form.js
+++ src/components/item-add-form/item-add-form.js
@@ -3,15 +3,35 @@ import React, {Component} from 'react';
 import './item-add-form.css';
 
 class ItemAddForm extends Component {
+
+    state = {
+        label: ""
+    };
+    onLabelChange = (e) => {
+        this.setState({
+            label: e.target.value
+        });
+    };
+
+    onSubmit = (e) => {
+        e.preventDefault();
+        this.props.onItemAdded(this.state.label);
+    }
     render() {
         return (
-            <div className="item-add-form">
+            <form className="item-add-form d-flex"
+                    onSubmit={this.onSubmit}>
+          
+                <input type="text" 
+                        className="form-control"
+                        onChange={this.onLabelChange}
+                        placeholder="What needs to be done" />
                 <button type="button"
                     className="btn btn-outline-secondary"
-                    onClick={() => this.props.onItemAdded('Hello World')}>
+                    >
                     Add Item
                 </button>
-            </div>
+            </form>
         )
     }
 }
