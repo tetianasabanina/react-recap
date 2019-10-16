5e5195da37e2a05e2baac46af120393deef3ff44
Sabanina Tetiana <tatiana08st@gmail.com>
2019-10-16 20:14:25 +0300

    Added: search functionality

--- src/components/item-add-form/item-add-form.js
+++ src/components/item-add-form/item-add-form.js
@@ -9,7 +9,7 @@ class ItemAddForm extends Component {
     };
     onLabelChange = (e) => {
         this.setState({
-            label: e.target.value.toUpperCase()
+            label: e.target.value
         });
     };
 
@@ -23,16 +23,13 @@ class ItemAddForm extends Component {
     render() {
         return (
             <form className="item-add-form d-flex"
-                    onSubmit={this.onSubmit}>
-          
+                    onSubmit={this.onSubmit}>          
                 <input type="text" 
                         className="form-control"
                         onChange={this.onLabelChange}
                         placeholder="What needs to be done" 
-                        value={this.state.label}/>
-                <button type="button"
-                    className="btn btn-outline-secondary"
-                    >
+                        value={this.state.label} required/>
+                <button className="btn btn-outline-secondary">
                     Add Item
                 </button>
             </form>
--- src/components/search-panel/search-panel.js
+++ src/components/search-panel/search-panel.js
@@ -1,17 +1,28 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 import './search-panel.css';
 
+class SearchPanel extends Component {
 
-const SearchPanel = () => {
-    return (
-        <div>
+    state = {
+        term: ''
+    };
+
+    onSearchChange = (e) => {
+        const term = e.target.value;
+        this.setState({ term });
+        this.props.onSearchChange(term);
+    };
+    render() {
+        return (
             <input type="text"
-            className="form-control search-input"
-            placeholder="type to search" />
-        </div>
-    );
-    
+                className="form-control search-input"
+                placeholder="type to search" 
+                value={this.state.term} 
+                onChange = {this.onSearchChange}/>
+            
+        );
+    }; 
 };
 
 export default SearchPanel;
