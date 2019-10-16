194806340d7783718e27cca830003ee099266a79
Sabanina Tetiana <tatiana08st@gmail.com>
2019-10-16 21:01:13 +0300

    Updates

---
+++ precache-manifest.deb16ddc88f630d797ee3b6a9d77ebae.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c07b49ddbad64a44e141afa9f643a625",
+    "url": "/react-recap/index.html"
+  },
+  {
+    "revision": "fed59dc3e81cf356f3c6",
+    "url": "/react-recap/static/css/main.00c7e323.chunk.css"
+  },
+  {
+    "revision": "bad49cc759fa37d723ac",
+    "url": "/react-recap/static/js/2.2df12623.chunk.js"
+  },
+  {
+    "revision": "fed59dc3e81cf356f3c6",
+    "url": "/react-recap/static/js/main.19fb29fc.chunk.js"
+  },
+  {
+    "revision": "b0de5a7b057e311d50f6",
+    "url": "/react-recap/static/js/runtime-main.9797f79e.js"
+  }
+]);
